a32feeebb1ab25144d63519add3edc8c157ef949
ARXINO <arxtechnologies@gmail.com>
2022-06-05 11:11:02 +0300

    Database login, register added

--- Database/index.js
+++ Database/index.js
@@ -1,11 +1,26 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
+const userRoute = require("./routes/users");
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose.connect("mongodb+srv://MicroserviceProject:mongo.123@microserviceproject.gbycla6.mongodb.net/?retryWrites=true&w=majority", {
+
+    useNewUrlParser: true,
+  
+    useUnifiedTopology: true,
+  
+  }).then(() => console.log("Database connected!"))
+  
+    .catch(err => console.log(err));
 
 app.post("/api/login", (req, res) => {
     res.send("hello from database");
 });
+
+app.use("/api/users", userRoute);
 
 app.get("*", (req, res) => {
     res.status(404).send("Get API not found.");
---
+++ Database/routes/users.js
@@ -0,0 +1,62 @@
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user")
+
+//register
+router.post("/register", async (req, res) => {
+  try {
+    const checkUser = await User.findOne({ username: req.body.username });
+    if (checkUser) {
+      return res.status(404).json("user already exists");
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    
+    //create new user
+    const newUser = new User({
+      username: req.body.username,
+      password: hashedPassword,
+    });
+
+    //save user and respond
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (err) {
+      console.log(err);
+    res.status(500).json(err)
+  }
+});
+
+//LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) {
+      return res.status(404).json("user not found");
+
+    }
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password)
+    if (!validPassword) {
+      return res.status(400).json("wrong password");
+
+    }
+
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+      }, "microserviceapp", //secret key
+      { expiresIn: "3d" }
+    );
+
+    const { password, ...others } = user._doc;
+    res.status(200).json({ ...others, accessToken });
+
+
+  } catch (err) {
+    res.status(500).json(err)
+  }
+});
+
+module.exports = router;
